87643d3a760fed7aab6f282688739aef28888828
shintalha <sahintalha0@outlook.com>
2023-09-14 15:49:47 +0300

    first commit

---
+++ src/main/java/com/personal/stockmarketmanagement/StockMarketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.personal.stockmarketmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/stockmarketmanagement/StockMarketManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.stockmarketmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
